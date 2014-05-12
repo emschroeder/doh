12b1f85c5d8217531bbe108d53eb391ccfa1de74
emschroeder <eschroeder@engr-134-19.eastern.ewu.edu>
2014-05-12 13:49:27 -0700

    Video Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 // My Scripts
 $(document).foundation();
+
+
+
+// BigVideo
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('http://emschroeder.github.io/doh/oceans.mp4');
+});
